400a79645baf8ce0e690e74e46dc7595ff6211ec
Arjun <gt01arjun@thecea.ca>
2021-06-12 22:46:21 -0700

    Added Car Controller

---
+++ Assets/Scripts/CarMovement.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _accelerationPower;
+    [SerializeField]
+    private float _steeringPower;
+
+    private float steeringAmount;
+    private float _speed;
+    private float _direction;
+
+    private Rigidbody2D _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        steeringAmount = -Input.GetAxis("Horizontal");
+        _speed = Input.GetAxis("Vertical") * _accelerationPower;
+        _direction = Mathf.Sign(Vector2.Dot(_rb.velocity, _rb.GetRelativeVector(Vector2.up)));
+        _rb.rotation += steeringAmount * _steeringPower * _rb.velocity.magnitude * _direction;
+
+        _rb.AddRelativeForce(Vector2.up * _speed);
+
+        _rb.AddRelativeForce(-Vector2.right * _rb.velocity.magnitude * steeringAmount / 2);
+    }
+}
