8391fe708fd213fbead6f2ddd7716c59058dc9c7
Arjun <gt01arjun@thecea.ca>
2021-06-14 20:44:50 -0700

    Added Player/Car Switch

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,8 +7,11 @@ public class PlayerMovement : MonoBehaviour
 
     private Rigidbody2D _rb;
     private Animator _animator;
-
     private Vector2 _movement;
+
+    private bool _canEnterCar;
+
+    private GameObject _currentCar;
 
     private void Start()
     {
@@ -24,10 +27,27 @@ public class PlayerMovement : MonoBehaviour
         _animator.SetFloat("Horizontal", _movement.x);
         _animator.SetFloat("Vertical", _movement.y);
         _animator.SetFloat("Speed", _movement.sqrMagnitude);
+
+        if (Input.GetKeyDown(KeyCode.E) && _canEnterCar)
+        {
+            _currentCar.GetComponent<CarMovement>().enabled = true;
+            _currentCar.GetComponent<CarMovement>().Player = gameObject;
+            _currentCar.GetComponent<CarMovement>().PlayerInsideCar = true;
+            gameObject.SetActive(false);
+        }
     }
 
     private void FixedUpdate()
     {
         _rb.MovePosition(_rb.position + _movement.normalized * _moveSpeed * Time.fixedDeltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<CarMovement>())
+        {
+            _canEnterCar = true;
+            _currentCar = collision.gameObject;
+        }
     }
 }
