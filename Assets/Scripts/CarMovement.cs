082e3ca2e6137c1a9b075aebacff1635a035a271
Arjun <gt01arjun@thecea.ca>
2021-06-17 18:45:55 -0700

    Added Gas Slider Functionality

--- Assets/Scripts/CarMovement.cs
+++ Assets/Scripts/CarMovement.cs
@@ -9,7 +9,7 @@ public class CarMovement : MonoBehaviour
     [SerializeField]
     private bool _canFillGas;
 
-    private float steeringAmount;
+    private float _steeringAmount;
     private float _speed;
     private float _direction;
 
@@ -28,7 +28,29 @@ public class CarMovement : MonoBehaviour
 
     private void Update()
     {
-        steeringAmount = -Input.GetAxis("Horizontal");
+        if (_canFillGas)
+        {
+            TargetCarMovement();
+        }
+        else
+        {
+            OtherCarMovement();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _direction = Mathf.Sign(Vector2.Dot(_rb.velocity, _rb.GetRelativeVector(Vector2.up)));
+        _rb.rotation += (int)(_steeringAmount * _steeringPower * _rb.velocity.magnitude * _direction);
+
+        _rb.AddRelativeForce(Vector2.up * _speed);
+
+        _rb.AddRelativeForce(-Vector2.right * _rb.velocity.magnitude * _steeringAmount / 2);
+    }
+
+    public void OtherCarMovement()
+    {
+        _steeringAmount = -Input.GetAxis("Horizontal");
         _speed = Input.GetAxis("Vertical") * _accelerationPower;
 
         if (Input.GetKeyDown(KeyCode.E) && PlayerInsideCar)
@@ -40,21 +62,38 @@ public class CarMovement : MonoBehaviour
             Player = null;
             gameObject.GetComponent<CarMovement>().enabled = false;
         }
-
-        if(Input.GetKeyDown(KeyCode.F) && _canFillGas && GameManager.GasCanCounter > 0)
-        {
-            Debug.Log("Fill");
-        }
     }
 
-    private void FixedUpdate()
+    public void TargetCarMovement()
     {
-        _direction = Mathf.Sign(Vector2.Dot(_rb.velocity, _rb.GetRelativeVector(Vector2.up)));
-        _rb.rotation += (int)(steeringAmount * _steeringPower * _rb.velocity.magnitude * _direction);
+        if (GameManager.CurrentGasAmount > 0f)
+        {
+            _steeringAmount = -Input.GetAxis("Horizontal");
+            _speed = Input.GetAxis("Vertical") * _accelerationPower;
 
-        _rb.AddRelativeForce(Vector2.up * _speed);
+            if (Mathf.Abs(_steeringAmount) > 0 || Mathf.Abs(_speed) > 0)
+            {
+                GameManager.CurrentGasAmount = Mathf.Clamp(GameManager.CurrentGasAmount - Time.deltaTime * 0.1f, 0f, 1f);
+            }
+        }
 
-        _rb.AddRelativeForce(-Vector2.right * _rb.velocity.magnitude * steeringAmount / 2);
+        if (Input.GetKeyDown(KeyCode.E) && PlayerInsideCar)
+        {
+            _mainCamera.GetComponent<CameraFollow>().TargetToFollow = Player.transform;
+            Player.transform.position = new Vector2(gameObject.transform.position.x + 2f, gameObject.transform.position.y);
+            Player.SetActive(true);
+            PlayerInsideCar = false;
+            Player = null;
+            gameObject.GetComponent<CarMovement>().enabled = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.F) && _canFillGas && GameManager.GasCanCounter > 0)
+        {
+            Debug.Log("Fill");
+            GameManager.CurrentGasAmount = 1;
+            GameManager.GasCanCounter--;
+            GameManager.TotalOilCans--;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,31 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static bool GameOver;
-
     public static int GasCanCounter;
+    public static int TotalOilCans;
+    public static float CurrentGasAmount;
+
+    [SerializeField]
+    private Slider _gasBar;
+
+    private void Start()
+    {
+        GameOver = false;
+        GasCanCounter = 0;
+        CurrentGasAmount = 0;
+        TotalOilCans = FindObjectsOfType<OilCan>().Length;
+    }
+
+    private void Update()
+    {
+        _gasBar.value = CurrentGasAmount;
+
+        if (CurrentGasAmount <= 0 && TotalOilCans <= 0)
+        {
+            Debug.Log("GameOver");
+        }
+    }
 }
